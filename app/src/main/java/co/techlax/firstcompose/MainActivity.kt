71d0b5aa6c7ec47d4612d971e9fd88546d058961
Shonu72 <shourya7250@gmail.com>
2023-09-22 16:10:08 +0530

    modifier

--- app/src/main/java/co/techlax/firstcompose/MainActivity.kt
+++ app/src/main/java/co/techlax/firstcompose/MainActivity.kt
@@ -6,10 +6,14 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,7 +21,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,20 +34,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 //                TextInput()
-            Column {
-                PreviewFunction(R.drawable.prof,"Shourya","Sde 1")
-                PreviewFunction(R.drawable.prof,"Shourya","Sde 1")
-                PreviewFunction(R.drawable.prof,"Shourya","Sde 1")
-                PreviewFunction(R.drawable.prof,"Shourya","Sde 1")
+//            Column {
+//                listViewFunction(R.drawable.prof,"Shourya","Sde 1")
+//                listViewFunction(R.drawable.prof,"Shourya","Sde 1")
+//                listViewFunction(R.drawable.prof,"Shourya","Sde 1")
+//                listViewFunction(R.drawable.prof,"Shourya","Sde 1")
+//            }
 
-            }
+            circlePreview()
         }
     }
 }
 
 //@Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
-fun PreviewFunction(imgId:Int, name:String, des:String){
+fun listViewFunction(imgId:Int, name:String, des:String, modifier: Modifier){
     // in case of column we arrange items in the vertical(ek ke niche ek) but in row we arrange them in horizontal(ek k baad ek)
 //    Column(
 //        verticalArrangement = Arrangement.Center,
@@ -60,10 +67,10 @@ fun PreviewFunction(imgId:Int, name:String, des:String){
 //        Text(text = "Y", fontSize = 24.sp)
 //    }
 
-    Row(Modifier.padding(8.dp)) {
+    Row(modifier.padding(8.dp)) {
         Image(painter = painterResource(id = imgId),
             contentDescription ="",
-            Modifier.size(80.dp)
+            Modifier.size(200.dp)
         )
         Column {
             Text(text = name, fontSize = 24.sp,
@@ -88,5 +95,31 @@ TextField(
     },
     label = { Text(text = "Enter input ")}
 )
+
+}
+@Composable
+private fun previewFUnction(){
+    Text(text = "Preview", fontSize = 20.sp,
+        color = Color.White,
+        modifier = Modifier
+            .clickable { }
+            .background(Color.Blue)
+            .size(200.dp)
+            .padding(10.dp)
+            .border(4.dp, Color.Red)
+            .clip(CircleShape)
+            .background(Color.Yellow)
+    )
+}
+
+
+@Composable
+fun circlePreview(){
+    Image(painter = painterResource(id = R.drawable.prof),
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.size(80.dp)
+            .clip(CircleShape)
+            .border(2.dp, Color.LightGray, CircleShape),
+        contentDescription = "" )
 
 }
